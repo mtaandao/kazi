7ef3abf13bc8d31ae3edcb3b715fd28e864d695e
Mtaandao <mtaandao@gmail.com>
2016-12-14 14:30:34 +0300

    Init
    Basic layout.

---
+++ app/src/main/java/io/mn/kazi/Tab2.java
@@ -0,0 +1,27 @@
+package io.mn.kazi;
+
+/**
+ * Created by SwahiliPot Hub on 12/14/2016.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+//Our class extending fragment
+public class Tab2 extends Fragment {
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
+
+    //Overriden method onCreateView
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        //Returning the layout file after inflating
+        //Change R.layout.tab1 in you classes
+        return inflater.inflate(R.layout.tab2, container, false);
+    }
+}
